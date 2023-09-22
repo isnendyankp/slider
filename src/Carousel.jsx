760c438dd983f5dcde823d3f9a9699f4cbd80d86
isnendyankp <isnendyankp@gmail.com>
2023-09-22 13:27:21 +0700

    s7-210:pass in shortList as default state value

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { shortList, list, longList } from './data';
 
 const Carousel = () => {
-  const [people, setPeople] = usestate;
+  const [people, setPeople] = usestate(shortList);
   return (
     <h2>Carousel</h2>
   )
@@ -14,3 +14,4 @@ export default Carousel
 // - s7-209:Change structure div into h2
 // - s7-209:Import shortlist, list & longlist
 // - s7-210:Cr8 people state value
+// - s7-210:pass in shortList as default state value @people state value
