803045a6f910f528e76b3f329d54249ba972069d
isnendyankp <isnendyankp@gmail.com>
2023-09-22 11:44:12 +0700

    s7-209:Pass in Carousel @main return

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Carousel from './Carousel';
 const App = () => {
   return (
     <main>
-      <h2>Slider Starter</h2>
+      <Carousel />
     </main>
   );
 };
 export default App;
 
 // - s7-209:import carousel @App.jsx
+// - s7-209:Pass in Carousel @main return
