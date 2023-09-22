b1e8a9b9d0e73a985e3f4bf95da0440e42835355
isnendyankp <isnendyankp@gmail.com>
2023-09-22 16:29:18 +0700

    s7-210:Add key base @return article

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -6,7 +6,7 @@ const Carousel = () => {
   return (
     <section className='slider-container'>
       {people.map((person)=>{
-        return <article className='slide'></article>
+        return <article className='slide' key={}></article>
       })}
     </section>
   )
@@ -24,3 +24,4 @@ export default Carousel
 // - s7-210:pass in person  @return iterate over people state value
 // - s7-210:Add return with article @people.map return
 // - s7-210:Add CN slide @return article
+// - s7-210:Add key base @return article
