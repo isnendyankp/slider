c16fc1049971c51fa708f9b2433e5903397bec0a
isnendyankp <isnendyankp@gmail.com>
2023-09-26 10:46:00 +0700

    s7-210:pass in FaQuoteRight

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -13,6 +13,7 @@ const Carousel = () => {
           <h5 className='name'>{name}</h5>
           <p className='title'>{title}</p>
           <p className='text'>{quote}</p>
+          <FaQuoteRight />
         </article>
       })}
     </section>
@@ -44,3 +45,4 @@ export default Carousel
 // - s7-210:pass in title @p with CN title
 // - s7-210:cr8 paragraph with CN text
 // - s7-210:pass in quote @p with CN text
+// - s7-210:import & pass in FaQuoteRight
