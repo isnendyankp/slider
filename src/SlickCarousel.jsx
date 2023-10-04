a343798a9437970ac578f6cb9c1d8a9be3e408af
isnendyankp <isnendyankp@gmail.com>
2023-10-04 12:36:54 +0700

    s7-216:Cr8 SlickCarousel component

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -5,9 +5,14 @@ import Slider from "react-slick";
 import { list } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 
+const SlickCarousel = () => {
+
+};
+
 // - s7-216:Cr8 SlickCarousel component
 // - s7-216:import SlickCarousel>slick css
 // - s7-216:import SlickCarousel>slick theme css
 // - s7-216:import Slider from react-slick
 // - s7-216:import list from data
 // - s7-216:import FaQuoteRight from react-icons/fa
+// - s7-216:Cr8 SlickCarousel component
