5a7e84eb590b00f13125447215e1324c715b6f7f
isnendyankp <isnendyankp@gmail.com>
2023-10-04 12:42:05 +0700

    s7-216:import slickCarousel @App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Carousel from './Carousel';
+import SlickCarousel from './SlickCarousel';
 
 const App = () => {
   return (
     <main>
       <Carousel />
+      
     </main>
   );
 };
@@ -11,3 +13,4 @@ export default App;
 
 // - s7-209:import carousel @App.jsx
 // - s7-209:Pass in Carousel @main return
+// - s7-216:import slickCarousel @App
