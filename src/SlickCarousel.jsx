b17ee0046dee89248732ab4d52c3eb2e3542c50c
isnendyankp <isnendyankp@gmail.com>
2023-10-04 12:32:49 +0700

    s7-216:import SlickCarousel>slick theme css

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -1,4 +1,6 @@
 import `slick-carousel/slick/slick.css`;
+import `slick-carousel/slick/slick-theme.css`;
 
 // - s7-216:Cr8 SlickCarousel component
-// - s7-216:import SlickCarousel>css
+// - s7-216:import SlickCarousel>slick css
+// - s7-216:import SlickCarousel>slick theme css
