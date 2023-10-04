fbd254b515fe61c3e578ef04182590cf39c330b6
isnendyankp <isnendyankp@gmail.com>
2023-10-04 12:33:45 +0700

    s7-216:import Slider from react-slick

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -1,6 +1,9 @@
 import `slick-carousel/slick/slick.css`;
 import `slick-carousel/slick/slick-theme.css`;
 
+import Slider from "react-slick";
+
 // - s7-216:Cr8 SlickCarousel component
 // - s7-216:import SlickCarousel>slick css
 // - s7-216:import SlickCarousel>slick theme css
+// - s7-216:import Slider from react-slick
