931eae3e4de200a008c82832452f98ccc8403bf0
isnendyankp <isnendyankp@gmail.com>
2023-10-04 12:35:26 +0700

    s7-216:import FaQuoteRight from react-icons/fa

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -3,9 +3,11 @@ import `slick-carousel/slick/slick-theme.css`;
 
 import Slider from "react-slick";
 import { list } from "./data";
+import { FaQuoteRight } from "react-icons/fa";
 
 // - s7-216:Cr8 SlickCarousel component
 // - s7-216:import SlickCarousel>slick css
 // - s7-216:import SlickCarousel>slick theme css
 // - s7-216:import Slider from react-slick
 // - s7-216:import list from data
+// - s7-216:import FaQuoteRight from react-icons/fa
