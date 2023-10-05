9275c614c6551bfdaf8066e2433faebdb76121b4
isnendyankp <isnendyankp@gmail.com>
2023-10-05 11:19:46 +0700

    Pass in Slider with settings

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -15,7 +15,9 @@ const SlickCarousel = () => {
     };
     return (
         <section className="slic-container">
-            <h2> Slick Slider </h2>;
+            <Slider {...settings}> 
+                
+            </Slider>
         </section>       
     );
 };
@@ -37,3 +39,4 @@ export default SlickCarousel;
 // - s7-216:Add speed:500 @SlickCarousel>settings
 // - s7-216:Add slidesToShow & slidesToScroll @SlickCarousel>settings
 // - s7-216:Cr8 section with className slic-container @SlickCarousel>return
+// - s7-216:Pass in Slider with settings @SlickCarousel>return
